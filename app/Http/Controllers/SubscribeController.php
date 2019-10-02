4220fba7b1ff9dde1d1c8106cb9f7b565c172d45
AlexNesvat <nonaname1993@gmail.com>
2019-10-02 12:07:19 +0300

    initial commit

---
+++ app/Http/Controllers/SubscribeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SubscribeRequest;
+
+class SubscribeController extends Controller
+{
+
+    public function subscribe(SubscribeRequest $request)
+    {
+        $data = $request->all();
+
+        
+    }
+}
